577e2871de9639234671d8e2a02cc40a27a87971
Justin Melore <melorejustin@gmail.com>
2025-01-22 10:49:04 -0700

    Made it so the light collider's angle and distance can be modified

--- Our Light/Assets/Scripts/PlayerLight.cs
+++ Our Light/Assets/Scripts/PlayerLight.cs
@@ -1,39 +1,50 @@
 using UnityEngine;
 
+/// <summary>
+/// Handles the player's light and its functionality
+/// </summary>
 public class PlayerLight : MonoBehaviour
 {
 
-    private int subdivisionCount = 1;
+    private int subdivisionCount = 30;
+    [SerializeField] private float angle;
+    [SerializeField] private float distance;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// <summary>
+    /// Creates the cone mesh that serves as the light's collision
+    /// </summary>
     void Start()
     {
         Mesh lightColliderMesh = new Mesh();
         GetComponent<MeshFilter>().mesh = lightColliderMesh;
+        transform.rotation = Quaternion.Euler(new Vector3(90f, -angle / 2, 0));
 
         Vector3[] vertices = new Vector3[subdivisionCount + 2];
         Vector2[] uvs = new Vector2[vertices.Length];
         int[] triangles = new int[subdivisionCount * 3];
+        vertices[0] = Vector3.zero;
+        float currentAngle = 0f;
+        float angleIncrement = angle / subdivisionCount;
 
+        for (int i = 0; i <= subdivisionCount; i++)
+        {
+            float angleRad = currentAngle * (Mathf.PI / 180f);
+            Vector3 vertex = new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad)) * distance;
+            vertices[i+1] = vertex;
 
-        vertices[0] = new Vector3(1, 0, 1);
-        vertices[1] = new Vector3(0, 1, 1);
-        vertices[2] = new Vector3(2, 1, 1);
+            if(i > 0)
+            {
+                triangles[i * 3 - 3] = 0;
+                triangles[i * 3 - 2] = i;
+                triangles[i * 3 - 1] = i + 1;
+            }
 
-        triangles[0] = 0;
-        triangles[1] = 1;
-        triangles[2] = 2;
-
-        //vertices[0] = Vector3.zero;
-        //for(int i = 0; i <= subdivisionCount; i++)
-        //{
-        //    Vector3
-        //}
+            currentAngle -= angleIncrement;
+        }
 
         lightColliderMesh.vertices = vertices;
         lightColliderMesh.uv = uvs;
         lightColliderMesh.triangles = triangles;
-
         GetComponent<MeshCollider>().sharedMesh = lightColliderMesh;
     }
 
