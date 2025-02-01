8918d9541efd0cd7f8ea9dd37df35f803a989888
Justin Melore <melorejustin@gmail.com>
2025-01-31 20:28:41 -0700

    Created checkpoints

---
+++ Our Light/Assets/Scripts/Checkpoints/Checkpoint.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private RespawnManager respawnManager;
+    private Vector3 respawnPoint;
+
+    private bool used;
+
+    private void Start()
+    {
+        used = false;
+        respawnManager = FindFirstObjectByType<RespawnManager>();
+        respawnPoint = transform.GetChild(0).transform.position;
+    }
+
+    private void OnTriggerEnter(UnityEngine.Collider other)
+    {
+        if(!used)
+        {
+            Debug.Log("Checkpoint reached");
+            respawnManager.SetRespawnPoint(respawnPoint);
+            respawnManager.ClearUnsavedLightables();
+            used = true;
+        }
+    }
+
+}
