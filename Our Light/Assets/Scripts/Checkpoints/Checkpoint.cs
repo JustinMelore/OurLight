c73110aa5584cf9b2a6b2c4e0b86bbe1294a2b1a
Justin Melore <melorejustin@gmail.com>
2025-02-25 17:57:34 -0700

    Made it so checkpoints can change to a different color upon being interacted with

--- Our Light/Assets/Scripts/Checkpoints/Checkpoint.cs
+++ Our Light/Assets/Scripts/Checkpoints/Checkpoint.cs
@@ -7,6 +7,12 @@ public class Checkpoint : MonoBehaviour
     private Vector3 respawnPoint;
     private PlayerLight playerLight;
 
+    private Light checkpointLight;
+    private Color startingColor;
+    [SerializeField] private Color usedColor;
+    private float currentColorChangeTime;
+    [SerializeField] private float colorChangeTime;
+
     private bool used;
 
     private void Start()
@@ -15,18 +21,29 @@ public class Checkpoint : MonoBehaviour
         respawnManager = FindFirstObjectByType<RespawnManager>();
         respawnPoint = transform.GetChild(0).transform.position;
         playerLight = FindFirstObjectByType<PlayerLight>();
+        checkpointLight = transform.GetComponentInChildren<Light>();
+        startingColor = checkpointLight.color;
+    }
+
+    private void Update()
+    {
+        if (!used || checkpointLight.color == usedColor) return;
+        currentColorChangeTime += Time.deltaTime;
+        Color currentColor = new Color();
+        currentColor.r = Mathf.Lerp(startingColor.r, usedColor.r, currentColorChangeTime / colorChangeTime);
+        currentColor.g = Mathf.Lerp(startingColor.g, usedColor.g, currentColorChangeTime / colorChangeTime);
+        currentColor.b = Mathf.Lerp(startingColor.b, usedColor.b, currentColorChangeTime / colorChangeTime);
+        checkpointLight.color = currentColor;
     }
 
     private void OnTriggerEnter(UnityEngine.Collider other)
     {
-        if(!used)
-        {
-            Debug.Log("Checkpoint reached");
-            respawnManager.SetRespawnPoint(respawnPoint);
-            respawnManager.ClearUnsavedLightables();
-            playerLight.ResetLight();
-            used = true;
-        }
+        if (used) return;
+        Debug.Log("Checkpoint reached");
+        respawnManager.SetRespawnPoint(respawnPoint);
+        respawnManager.ClearUnsavedLightables();
+        playerLight.ResetLight();
+        used = true;
     }
 
 }
