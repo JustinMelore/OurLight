6bbc349c3cd76eea154f355ba75b5187c9cca3bd
JustinMelore <melorejustin@gmail.com>
2025-03-19 16:25:24 -0600

    Made the bridge npc trigger a music shift

--- Our Light/Assets/Scripts/Lightables/LightableNPC.cs
+++ Our Light/Assets/Scripts/Lightables/LightableNPC.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class LightableNPC : Lightable
 {
     [SerializeField] private LightMode unlockableMode;
+    private AudioManager audioManager;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        audioManager = FindFirstObjectByType<AudioManager>();
+    }
 
     public override void ChangeLightableState(bool isRevealed)
     {
@@ -10,6 +17,7 @@ public class LightableNPC : Lightable
         if(isRevealed)
         {
             playerLight.UnlockMode(unlockableMode);
+            audioManager.SwitchBackgroundMusic();
             Destroy(this.gameObject);
         }
     }
