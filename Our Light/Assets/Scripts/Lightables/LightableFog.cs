c422dfab2f11ad7eb70242118b9687200d5c9362
JustinMelore <melorejustin@gmail.com>
2025-04-09 11:56:17 -0600

    Experimented with changing fog wall colors

--- Our Light/Assets/Scripts/Lightables/LightableFog.cs
+++ Our Light/Assets/Scripts/Lightables/LightableFog.cs
@@ -4,6 +4,13 @@ public class LightableFog : Lightable
 {
     [SerializeField] private Collider wall;
     [SerializeField] private ParticleSystem fog;
+    private ParticleSystem.MinMaxGradient startingFogColor;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        startingFogColor = fog.main.startColor;
+    }
 
     public override void ChangeLightableState(bool isRevealed)
     {
@@ -12,5 +19,21 @@ public class LightableFog : Lightable
         if (isRevealed) fog.Stop();
         else fog.Play();
     }
+
+    protected override void StartLighting()
+    {
+        base.StartLighting();
+        var main = fog.main;
+        main.startColor = new Color(1f, 233f / 255f, 0f);
+    }
+
+    protected override void StopLighting()
+    {
+        base.StopLighting();
+        var main = fog.main;
+        main.startColor = startingFogColor;
+    }
+
+
 
 }
