7f3d8c287ff15558ee124a7c2a0d97b7f71891fd
JustinMelore <melorejustin@gmail.com>
2025-04-15 12:30:05 -0600

    Added dialogue icon for when the child speaks

--- Our Light/Assets/Scripts/UI/DialogueBox.cs
+++ Our Light/Assets/Scripts/UI/DialogueBox.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.UI;
 
 public class DialogueBox : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class DialogueBox : MonoBehaviour
     private TextMeshProUGUI dialogueText;
     private PlayerLight playerLight;
     private PlayerController player;
+    [SerializeField] private GameObject dialogueIcon;
 
     void Awake()
     {
@@ -23,13 +25,15 @@ public class DialogueBox : MonoBehaviour
     private void Start()
     {
         gameObject.SetActive(false);
+        dialogueIcon.SetActive(false);
     }
 
-    public void StartDialogue(string[] dialogueList)
+    public void StartDialogue(string[] dialogueList, bool fromChild = false)
     {
         playerLight.TurnOffLight();
         playerLight.enabled = false;
         player.enabled = false;
+        if (fromChild) dialogueIcon.SetActive(true);
         gameObject.SetActive(true);
         EventSystem.current.SetSelectedGameObject(transform.Find("DialogueButton").gameObject);
         foreach(string dialogue in dialogueList) dialogueQueue.Enqueue(dialogue);
@@ -52,6 +56,7 @@ public class DialogueBox : MonoBehaviour
         boxDisplay.alpha = 0f;
         playerLight.enabled = true;
         player.enabled = true;
+        dialogueIcon.SetActive(false);
         gameObject.SetActive(false);
         EventSystem.current.SetSelectedGameObject(null);
     }
