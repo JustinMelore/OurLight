8f739e563cc369efacb09f91f891fc927fcd2f23
JustinMelore <melorejustin@gmail.com>
2025-04-15 13:03:54 -0600

    Created end screen in UI

---
+++ Our Light/Assets/Scripts/UI/EndScreen.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    private CanvasGroup[] badEndingText;
+    private CanvasGroup[] goodEndingText;
+    [SerializeField] private CanvasGroup endButton;
+    [SerializeField] private CanvasGroup fade;
+    [SerializeField] private float fadeTime;
+    [SerializeField] private float textRevealTime;
+    [SerializeField] private float buttonRevealTime;
+
+
+    void Awake()
+    {
+        badEndingText = transform.Find("BadEnding").GetComponentsInChildren<CanvasGroup>();
+        goodEndingText = transform.Find("GoodEnding").GetComponentsInChildren<CanvasGroup>();
+    }
+
+    public void ShowEndingScreen()
+    {
+        fade.gameObject.SetActive(true);
+        StartCoroutine(ShowEndScreenCoroutine());
+    }
+
+    private IEnumerator ShowEndScreenCoroutine()
+    {
+        yield return StartCoroutine(FadeInCanvasGroup(fade, fadeTime));
+        PlayerLight playerLight = FindFirstObjectByType<PlayerLight>();
+        CanvasGroup[] endingText = (playerLight.HasUnlockedMode(LightMode.BRIDGE) ? goodEndingText : badEndingText);
+        playerLight.enabled = false;
+        FindFirstObjectByType<PlayerController>().enabled = false;
+        float individualLineReveal = textRevealTime / endingText.Length;
+        foreach (CanvasGroup line in endingText) yield return StartCoroutine(FadeInCanvasGroup(line, individualLineReveal));
+        yield return new WaitForSeconds(buttonRevealTime);
+        endButton.gameObject.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(endButton.gameObject);
+        endButton.alpha = 1f;
+    }
+
+    private IEnumerator FadeInCanvasGroup(CanvasGroup group, float neededFadeTime)
+    {
+        float currentFadeTime = 0f;
+        while (group.alpha < 1f)
+        {
+            currentFadeTime += Time.deltaTime;
+            group.alpha = Mathf.Lerp(0f, 1f, currentFadeTime / neededFadeTime);
+            yield return null;
+        }
+    }
+
+    public void OnContinue()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
