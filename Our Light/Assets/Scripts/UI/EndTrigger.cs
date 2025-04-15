b346685e2fc77c35da557f2a5e2214d0f461a524
JustinMelore <melorejustin@gmail.com>
2025-04-15 13:26:19 -0600

    Created end screen and trigger

--- Our Light/Assets/Scripts/UI/EndTrigger.cs
+++ Our Light/Assets/Scripts/UI/EndTrigger.cs
@@ -4,6 +4,7 @@ public class EndTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        Cursor.lockState = CursorLockMode.Locked;
         FindFirstObjectByType<EndScreen>(FindObjectsInactive.Include).ShowEndingScreen();
     }
 }
